0cc6a3b734fe724ea9c704f620901a258cd9d4a4
Jamaludin Rajalu <jrajalu@gmail.com>
2016-07-21 23:34:47 +0800

    No commit message

--- fonts/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * @package UKMTheme
- * @subpackage UKM Twenty Fifteen
- * @version 1.0
- * @author Jamaludin Rajalu
- */
--- js/src/admin.js
+++
@@ -1,8 +0,0 @@
-/**
- * @package UKMTheme
- * @subpackage UKM Twenty Fifteen
- * @since 1.0
- */
-jQuery(document).ready(function($){
-  //
-});

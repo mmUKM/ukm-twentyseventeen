7c398c3d877ee107d0d22669f9bc0656ced5d94d
jrajalu <jrajalu@gmail.com>
2025-06-26 16:39:19 +0800

    No commit message

--- templates/archive-kelestarian.php
+++ templates/archive-kelestarian.php
@@ -6,24 +6,30 @@
 get_header(); ?>
 <div class="wrap">
     <article class="uk-padding">
+        
         <h2><?php _e( 'Kelestarian', 'ukmtheme' ) ; ?></h2>
-    <?php
-
-        $query = new WP_Query( array( 
-        'post_type'       => 'kelestarian', 
-        'posts_per_page'  => -1, 
-        'orderby'         => 'menu_order', 
-        'order'           => 'ASC' 
-        ));
-
-    if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
         <div class="uk-padding" uk-grid>
-            <div class="uk-width-3-4">
-                <?php the_title( '<h3>', '</h3>'); ?>
-        <?php endwhile; else: ?>
-            <p><?php _e( 'Sorry, no posts matched your criteria.', 'ukmtheme' ); ?></p>  
-        <?php endif; ?>
-            <p><?php get_template_part( 'templates/content', 'paginate' ); ?></p>
+                <?php
+
+                    $query = new WP_Query( array( 
+                    'post_type'       => 'kelestarian', 
+                    'posts_per_page'  => -1, 
+                    'orderby'         => 'menu_order', 
+                    'order'           => 'ASC' 
+                    ));
+
+                if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+            
+                <div class="uk-width-3-4">
+                    <a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
+                </div>
+                    <?php endwhile; else: ?>
+                        <p><?php _e( 'Sorry, no posts matched your criteria.', 'ukmtheme' ); ?></p>  
+                    <?php endif; ?>
+                    
+            
+        </div>
+        <p><?php get_template_part( 'templates/content', 'paginate' ); ?></p>
     </article>
 </div>
 <?php get_footer(); ?>
